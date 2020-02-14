136c884a372f1a726c30b4b09977b476d4f0a1c5
monikappv <monika.popova@datopian.com>
2020-02-14 17:56:59 +0200

    [config][m]: Add Specifications + dropdown, Changelog, Patterns and Implementation to the navigation bar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -18,7 +18,28 @@ module.exports = {
     editLinks: true,
     sidebar: 'auto',
     nav: [
-      { text: 'Home', link: '/' }
+      { text: 'Home', link: '/' },
+      {
+        text: "Specifications",
+        items: [
+          { text: "CSV Dialect", link: "/specifications/csv-dialect/" },
+          { text: "Data Package", link: "/specifications/data-package/" },
+          { text: "Data Package Identifier", link: "/specifications/data-package-identifier/" },
+          { text: "Data Resource", link: "/specifications/data-resource/" },
+          { text: "Fiscal Data Package", link: "/specifications/fiscal-data-package/" },
+          { text: "Fiscal Data Package - Budgets", link: "/specifications/fiscal-data-package--budgets/" },
+          { text: "Fiscal Data Package - Spending", link: "/specifications/fiscal-data-package--spending/" },
+          { text: "Profiles", link: "/specifications/profiles/" },
+          { text: "Security", link: "/specifications/security/" },
+          { text: "Table Schema", link: "/specifications/table-schema/" },
+          { text: "Tabular Data Package", link: "/specifications/tabular-data-package/" },
+          { text: "Tabular Data Resource", link: "/specifications/tabular-data-resource/" },
+          { text: "Views", link: "/specifications/views/" },
+        ]
+      },
+      { text: 'Changelog', link: '/changelog/' },
+      { text: 'Patterns', link: '/patterns/' },
+      { text: 'Implementation', link: '/implementation/' }
     ]
   }
 }
