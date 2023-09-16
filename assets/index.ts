76823d68883a51810a0fdbc69f704eaa12b2bbda
roll <eskarev@gmail.com>
2023-09-16 15:54:37 +0100

    Normalized assets

---
+++ assets/index.ts
@@ -0,0 +1,70 @@
+import bcodmo from "./adoption/bcodmo.png"
+import cambridge from "./adoption/cambridge.png"
+import causanatura from "./adoption/causanatura.png"
+import chicago from "./adoption/chicago.png"
+import cmoa from "./adoption/cmoa.png"
+import cmso from "./adoption/cmso.png"
+import dataRetriever from "./adoption/data-retriever.png"
+import dataworld from "./adoption/data-world.png"
+import dataship from "./adoption/dataship.png"
+import dm4t from "./adoption/dm4t.png"
+import dryad from "./adoption/dryad.png"
+import elife from "./adoption/elife.png"
+import etalab from "./adoption/etalab.png"
+import eucom from "./adoption/eucom.png"
+import hubmap from "./adoption/hubmap.png"
+import johnSnowLabs from "./adoption/john-snow-labs.png"
+import librariesHacked from "./adoption/libraries-hacked.png"
+import nimblelearn from "./adoption/nimblelearn.png"
+import odb from "./adoption/odb.png"
+import openml from "./adoption/openml.png"
+import opsd from "./adoption/opsd.png"
+import owid from "./adoption/owid.png"
+import oxford from "./adoption/oxford.png"
+import pnnl from "./adoption/pnnl.png"
+import pudl from "./adoption/pudl.png"
+import tesera from "./adoption/tesera.png"
+import ukds from "./adoption/ukds.png"
+import uop from "./adoption/uop.png"
+import validata from "./adoption/validata.png"
+import zegami from "./adoption/zegami.png"
+import dataCurator from "./software/data-curator.png"
+import odet from "./software/odet.png"
+
+export const adoption = {
+  bcodmo,
+  cambridge,
+  causanatura,
+  chicago,
+  cmoa,
+  cmso,
+  dataRetriever,
+  dataship,
+  dataworld,
+  dm4t,
+  dryad,
+  elife,
+  etalab,
+  eucom,
+  hubmap,
+  johnSnowLabs,
+  librariesHacked,
+  nimblelearn,
+  odb,
+  openml,
+  opsd,
+  owid,
+  oxford,
+  pnnl,
+  pudl,
+  tesera,
+  ukds,
+  uop,
+  validata,
+  zegami,
+}
+
+export const software = {
+  odet,
+  dataCurator,
+}
