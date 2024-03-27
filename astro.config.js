b900f6673e187881d681b6ceed1d5534fbae6469
roll <eskarev@gmail.com>
2024-03-27 17:57:54 +0000

    Added anchor links

--- astro.config.js
+++ astro.config.js
@@ -1,5 +1,7 @@
+import { rehypeHeadingIds } from "@astrojs/markdown-remark"
 import starlight from "@astrojs/starlight"
 import { defineConfig } from "astro/config"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
 
 // https://astro.build/config
 export default defineConfig({
@@ -98,4 +100,16 @@ export default defineConfig({
       ],
     }),
   ],
+  markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+      [
+        rehypeAutolinkHeadings,
+        {
+          // Wrap the heading text in a link.
+          behavior: "wrap",
+        },
+      ],
+    ],
+  },
 })
