52a8a0c78e9bb5ef25df38e250a74d1185dbf844
roll <eskarev@gmail.com>
2024-10-22 09:02:51 +0100

    Fixed GBIF image; added to the main page

--- assets/index.ts
+++ assets/index.ts
@@ -12,6 +12,7 @@ import dryad from "./adoption/dryad.png"
 import elife from "./adoption/elife.png"
 import etalab from "./adoption/etalab.png"
 import eucom from "./adoption/eucom.png"
+import gbif from "./adoption/gbif.png"
 import github from "./adoption/github.png"
 import hubmap from "./adoption/hubmap.png"
 import johnSnowLabs from "./adoption/john-snow-labs.png"
@@ -49,6 +50,7 @@ export const adoption = {
   elife,
   etalab,
   eucom,
+  gbif,
   github,
   hubmap,
   johnSnowLabs,
